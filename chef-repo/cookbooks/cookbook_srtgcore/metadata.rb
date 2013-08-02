588c0da84bf3a5aaffa445d8e691a988bb15ccb4
oshyuni <oshyuni@infranics.com>
2013-08-02 10:16:23 +0900

    cookbook_srtgcore
    sample

--- chef-repo/cookbooks/cookbook_srtgcore/metadata.rb
+++ chef-repo/cookbooks/cookbook_srtgcore/metadata.rb
@@ -1,7 +1,9 @@
 name             'cookbook_srtgcore'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'INFRANICS'
+maintainer_email 'oshyuni@infranics.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook_srtgcore'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "cookbook_srtgcore::default","Prints hello world output"
